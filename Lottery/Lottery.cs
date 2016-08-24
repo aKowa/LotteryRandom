e703d818c9020889f48284509197d263475af983
Andre Kowalewski <a.kowa@gmx.de>
2016-08-24 12:22:58 +0200

    [INITIALIZATION] from MemoRandom

---
+++ Lottery/Lottery.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kowa
+{
+	namespace MemoRandom
+	{
+		public class Lottery<T>
+		{
+			public IList<T> Collection { get; private set; }
+			public List<T> CollectionCopy { get; private set; }
+
+			/// <summary>
+			/// Constructor: Sets Collection and copies them.
+			/// </summary>
+			/// <param name="newCollection">The passed generic list</param>
+			public Lottery(IList<T> newCollection)
+			{
+				Collection = newCollection;
+				CopyEntries();
+			}
+
+			/// <summary>
+			/// Copies all entries
+			/// </summary>
+			private void CopyEntries()
+			{
+				CollectionCopy = Collection.ToList();
+			}
+
+			/// <summary>
+			/// Gets and removes the next random entry. Refills on empty.
+			/// </summary>
+			/// <returns>Returns next entry</returns>
+			public T DrawNext()
+			{
+				return DrawNext(LotteryMode.RefillOnEmpty);
+			}
+			
+			/// <summary>
+			/// Overload: Gets the next random entry. Refills or 
+			/// </summary>
+			/// <param name="mode">The mode to determine how to behave, when copied list is empty</param>
+			/// <returns>Returns next entry or default(T), depending on mode</returns>
+			public T DrawNext(LotteryMode mode)
+			{
+				if ( IsEmtpy () )
+				{
+					switch ( mode )
+					{
+						case LotteryMode.RefillOnEmpty:
+							CopyEntries ();
+							break;
+						case LotteryMode.RemoveOnEmpty:
+							LotteryManager<T>.Remove ( Collection );
+							return default ( T );
+						case LotteryMode.None:
+							return default ( T );
+					}
+				}
+				var id = Random.Range ( 0, CollectionCopy.Count );
+				var temp = CollectionCopy[id];
+				CollectionCopy.Remove ( CollectionCopy[id] );
+				return temp;
+			}
+			
+			/// <summary>
+			/// Determines whether the copied list is empty
+			/// </summary>
+			/// <returns>Returns True, if copied list is empty</returns>
+			public bool IsEmtpy()
+			{
+				return CollectionCopy.Count == 0;
+			}
+		}
+	}
+}
---
+++ Lottery/LotteryManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kowa
+{
+	namespace MemoRandom
+	{
+		/// <summary>
+		/// Manages all lists that called the lottery functionality.
+		/// </summary>
+		public static class LotteryManager<T>
+		{
+			private static readonly List<Lottery<T>> Dictionary = new List<Lottery<T>>();
+
+			/// <summary>
+			/// Method to be called to get the next random entry from passed collection. Adds or gets the passed collection from dictionary.
+			/// </summary>
+			/// <param name="collection">The collection to get the next random entry from. Reference Type from dictionary</param>
+			/// <returns>Next entry from passed collection.</returns>
+			// Adds the passed collection to the dictionary if its not in it
+			// Returns next element from the passed IList<T> that has not been returned previously
+			public static T DrawNext(IList<T> collection)
+			{
+				if (Dictionary.Contains(collection))
+				{
+					return Dictionary.Get(collection).DrawNext();
+				}
+				Dictionary.Add(new Lottery<T>(collection));
+				Debug.Log("Added " + collection.GetType() + " to dictionary");
+				return Dictionary.Get(collection).DrawNext();
+			}
+
+			/// <summary>
+			/// Indicates when all collection in passed collection have been returned in DrawNext once.
+			/// </summary>
+			/// <param name="collection">The collection to check if its empty.</param>
+			/// <returns>True, if all element from the passed collection have been accessed once</returns>
+			public static bool IsEmpty(IList<T> collection)
+			{
+				var entry = Dictionary.Get(collection);
+				return entry != null && entry.IsEmtpy();
+			}
+
+			/// <summary>
+			/// Removes the passed list from the dictionary.
+			/// </summary>
+			/// <param name="collection">The list that should be removed.</param>
+			public static void Remove(IList<T> collection)
+			{
+				Dictionary.Remove(Dictionary.Get(collection));
+			}
+
+			/// <summary>
+			/// Removes all added collection from the dictionary.
+			/// </summary>
+			public static void Clear()
+			{
+				Dictionary.Clear();
+			}
+		}
+	}
+}
